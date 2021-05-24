290351aa0aeb10271d740a304e133619136148fe
LuisDanielCova <ldcn96@gmail.com>
2021-05-24 19:18:45 -0400

    Primera version de la aplicacion movida a Github

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+const mongoDB = `mongodb://localhost:27017/test1`
+mongoose.connect(
+    mongoDB, 
+    {
+        useNewUrlParser: true, useUnifiedTopology: true
+    });
+
+app.listen(3001, ()=>{
+    console.log(`Servidor en ejecucion en el puerto 3001`);
+});
---
+++ server/models/Archivo.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let ArchivoSchema = Schema(
+    {
+        nombre: {type: String, required: true},
+        alumno: {type: Schema.Types.ObjectId, ref: 'Alumno', required: true}
+    }
+);
+
+ArchivoSchema
+    .virtual('url')
+    .get(() => {
+        `archivo/${this.id}`
+    });
+
+module.exports = mongoose.model('Archivo', ArchivoSchema);
